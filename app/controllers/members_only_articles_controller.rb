a6f6aea0cdb7dd8fa3b8f35be058f51e9e41b707
Samantha Navarro <samantha.m.navarro@gmail.com>
2022-07-27 19:23:03 -0700

    commit tree abf517f4c3d43f245c89331d851ccd8249c72397

--- app/controllers/members_only_articles_controller.rb
+++ app/controllers/members_only_articles_controller.rb
@@ -1,9 +1,12 @@
 class MembersOnlyArticlesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  before_action :authorize
 
   def index
-    articles = Article.where(is_member_only: true).includes(:user).order(created_at: :desc)
-    render json: articles, each_serializer: ArticleListSerializer
+    # articles = Article.where(is_member_only: true).includes(:user).order(created_at: :desc)
+    # render json: articles, each_serializer: ArticleListSerializer
+    articles = Article.all
+    render json: articles
   end
 
   def show
@@ -11,8 +14,13 @@ class MembersOnlyArticlesController < ApplicationController
     render json: article
   end
 
+  
   private
 
+  def authorize
+    return render json: {error: "Not authorized"}, status: :unauthorized
+  end
+  
   def record_not_found
     render json: { error: "Article not found" }, status: :not_found
   end
